485c2965b4c06b3db4df83b44992d1661850b9e3
suzuu <sukoratoshka@gmail.com>
2025-07-10 11:51:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,34 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      // Don't bundle modules that reference browser-incompatible APIs.
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false,
+      };
+    }
+
+    return config;
+  },
+};
+
+export default nextConfig;
